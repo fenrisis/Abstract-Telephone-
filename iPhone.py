b4a288de9e20158da35497cb412ebdf28b4c44ca
Leonid3# <116304748+fenrisis@users.noreply.github.com>
2024-01-10 18:34:27 +0100

    iPhone 15 & 4 classes

--- iPhone.py
+++ iPhone.py
@@ -11,3 +11,62 @@ class iPhone(Telephone):
     def send_message(self, number, message):
         return f"iPhone sending message to {number}: {message}"
 
+
+class IPhone15(iPhone):
+    def __init__(self, color, battery_capacity, serial_number):
+        super().__init__("15", color, battery_capacity)
+        self._serial_number = serial_number
+
+    @property
+    def serial_number(self):
+        return self._serial_number
+
+    @serial_number.setter
+    def serial_number(self, value):
+        self._serial_number = value
+
+    @staticmethod
+    def face_id_authentication():
+        return "Face ID authentication successful."
+
+    def get_info(self):
+        return (f"Brand: {self.brand}, "
+                f"Model: {self.model}, "
+                f"Color: {self.color}, "
+                f"Battery Capacity: {self.battery_capacity}mAh, "
+                f"OS: {self.os}, "
+                f"Serial Number: {self._serial_number}")
+
+
+class IPhone4(iPhone):
+    def __init__(self, color, battery_capacity):
+        super().__init__("4", color, battery_capacity)
+
+    @staticmethod
+    def classic_design():
+        return "This is the classic iPhone 4 design."
+
+    def get_info(self):
+        return (f"Brand: {self.brand}, "
+                f"Model: {self.model}, "
+                f"Color: {self.color}, "
+                f"Battery Capacity: {self.battery_capacity}mAh, "
+                f"OS: {self.os}, "
+                f"Design: {self.classic_design()}")
+
+
+iphone15 = IPhone15("Black", 3400, "SN123456")
+iphone4 = IPhone4("White", 1420)
+
+
+print(iphone15.face_id_authentication())
+print(iphone4.classic_design())
+
+print(f"Initial serial number: {iphone15.serial_number}")
+iphone15.serial_number = "SN654323"
+print(f"Updated serial number: {iphone15.serial_number}")
+
+print(iphone15.get_info())
+print(iphone4.get_info())
+
+
